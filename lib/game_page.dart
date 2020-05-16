dd53a5dc9337f5accaefd9313345785f87332ef2
Burhan <burhanhaq3@gmail.com>
2020-05-16 07:15:58 -0700

    Renamed Home to GamePage, and created another Home.dart

---
+++ lib/game_page.dart
@@ -0,0 +1,265 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'column_area.dart';
+import 'constants.dart';
+import 'piece_home.dart';
+import 'game_state.dart';
+import 'dice.dart';
+import 'triangle_painter.dart';
+import 'player_piece.dart';
+import 'fire_helper.dart';
+
+class GamePage extends StatefulWidget {
+  @override
+  _GamePageState createState() => _GamePageState();
+}
+
+class _GamePageState extends State<GamePage> {
+  final middleAreaSize = (kBoxWidth + kBoxBorderWidth * 2) * 3;
+
+  void listenToList() {
+    Fire.instance.listenForNewEverything().listen((data) {
+      var updated = data;
+      if (updated != null && updated.length > 0) {
+        print(updated);
+      }
+    });
+  }
+
+  List<Color> selectedDiceList;
+  List<Widget> diceMovesList;
+
+  generateSelectedDiceList(GameState gameState) {
+    selectedDiceList = List.generate(gameState.movesList.length, (i) {
+      if (i == gameState.selectedDiceIndex) return white;
+      return trans;
+    });
+  }
+
+  generateDiceMovesList(GameState gameState, Color c) {
+    if (gameState.selectedDiceIndex > gameState.movesList.length - 1) {
+      gameState.selectedDiceIndex = 0;
+    }
+    diceMovesList = List.generate(
+      gameState.movesList.length,
+      (index) => Stack(
+        alignment: Alignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.crop_free,
+            color: selectedDiceList[index],
+            size: kSmallDiceSize + kSelectedIconSize,
+          ),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                gameState.selectedDiceIndex = index;
+              });
+            },
+            child: Dice(
+              size: kSmallDiceSize,
+              c: c,
+              num: gameState.movesList[index],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  getRowChild(GameState gameState, PieceType pt) {
+    List<PlayerPiece> completedPiecesList = gameState
+        .getPlayerPieceList(pt)
+        .where((element) => element.isRunComplete()).toList();
+
+    int rowLength = completedPiecesList.length;
+    var topPiece = Container();
+    if (completedPiecesList.length == 4) {
+      --rowLength;
+      topPiece = Container(child: completedPiecesList.last.container);
+    }
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        topPiece,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(
+            rowLength,
+            (index) => Container(child: completedPiecesList[index].container),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var gameState = Provider.of<GameState>(context);
+    Color stateColor = PlayerPiece.getColor(gameState.getTurn());
+    generateSelectedDiceList(gameState);
+    generateDiceMovesList(gameState, stateColor);
+    return SafeArea(
+      child: Container(
+        color: grey,
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            maxHeight: (kBoxWidth) * 15,
+            maxWidth: (kBoxWidth) * 15,
+          ),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: MediaQuery.of(context).size.height * 0.04),
+              SizedBox(
+                height: (kBoxWidth + kBoxBorderWidth * 2) * 15,
+                width: (kBoxWidth + kBoxBorderWidth * 2) * 15,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: <Widget>[
+                    Positioned(
+                      top: 0,
+                      left: 0,
+                      child: PieceHome(PieceType.Blue),
+                    ),
+                    Positioned(
+                      top: 0,
+                      child: RotatedBox(
+                        quarterTurns: 2,
+                        child: ColumnArea(piece: PieceType.Red),
+                      ),
+                    ),
+                    Positioned(
+                      top: 0,
+                      right: 0,
+                      child: PieceHome(PieceType.Red),
+                    ),
+                    Positioned(
+                      left: 0,
+                      child: RotatedBox(
+                        quarterTurns: 1,
+                        child: ColumnArea(piece: PieceType.Blue),
+                      ),
+                    ),
+                    SizedBox(
+                      height: middleAreaSize,
+                      width: middleAreaSize,
+                      child: CustomPaint(
+                        size: Size(middleAreaSize, middleAreaSize),
+                        painter: TrianglePainter(
+                            c: PlayerPiece.getColor(PieceType.Green)),
+                        child: getRowChild(gameState, PieceType.Green),
+                      ),
+                    ),
+                    RotatedBox(
+                      quarterTurns: 1,
+                      child: SizedBox(
+                        height: middleAreaSize,
+                        width: middleAreaSize,
+                        child: CustomPaint(
+                          size: Size(middleAreaSize, middleAreaSize),
+                          painter: TrianglePainter(
+                              c: PlayerPiece.getColor(PieceType.Blue)),
+                          child: getRowChild(gameState, PieceType.Blue),
+                        ),
+                      ),
+                    ),
+                    RotatedBox(
+                      quarterTurns: 2,
+                      child: SizedBox(
+                        height: middleAreaSize,
+                        width: middleAreaSize,
+                        child: CustomPaint(
+                          size: Size(middleAreaSize, middleAreaSize),
+                          painter: TrianglePainter(
+                              c: PlayerPiece.getColor(PieceType.Red)),
+                          child: getRowChild(gameState, PieceType.Red),
+                        ),
+                      ),
+                    ),
+                    RotatedBox(
+                      quarterTurns: 3,
+                      child: SizedBox(
+                        height: middleAreaSize,
+                        width: middleAreaSize,
+                        child: CustomPaint(
+                          size: Size(middleAreaSize, middleAreaSize),
+                          painter: TrianglePainter(
+                              c: PlayerPiece.getColor(PieceType.Yellow)),
+                          child: getRowChild(gameState, PieceType.Yellow),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      right: 0,
+                      child: RotatedBox(
+                        quarterTurns: -1,
+                        child: ColumnArea(piece: PieceType.Yellow),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      left: 0,
+                      child: PieceHome(PieceType.Green),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      child: RotatedBox(
+                        quarterTurns: 0,
+                        child: ColumnArea(piece: PieceType.Green),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: PieceHome(PieceType.Yellow),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              SizedBox(
+                height: kSmallDiceSize + kSelectedIconSize,
+                width: MediaQuery.of(context).size.width,
+                child: ListView(
+                  physics: BouncingScrollPhysics(),
+                  scrollDirection: Axis.horizontal,
+                  children: diceMovesList,
+                ),
+              ),
+              SizedBox(height: 10),
+              GestureDetector(
+                onTap: gameState.diceTap,
+                child: Dice(
+                    size: 100, c: stateColor, num: gameState.lastMoveOnDice),
+              ),
+//              GestureDetector(
+//                onTap: Fire.instance.run,
+//                child: Container(
+//                  color: blue,
+//                  height: 200,
+//                  width: 200,
+//                  child: StreamBuilder(
+//                    stream: Fire.instance.gameStream('1589373131638'),
+//                    builder: (BuildContext context,
+//                        AsyncSnapshot<DocumentSnapshot> snapshot) {
+//                      if (snapshot.hasData) {
+//                        var x = snapshot.data.data['moves_list'];
+//                        return ListView(
+//                          children: [Text(x)],
+//                        );
+//                      }
+//                      return Text('nothing to show');
+//                    },
+//                  ),
+//                ),
+//              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/home.dart
+++ lib/home.dart
@@ -11,6 +11,7 @@ import 'dice.dart';
 import 'triangle_painter.dart';
 import 'player_piece.dart';
 import 'fire_helper.dart';
+import 'game_page.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -18,248 +19,73 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final middleAreaSize = (kBoxWidth + kBoxBorderWidth * 2) * 3;
-
-  void listenToList() {
-    Fire.instance.listenForNewEverything().listen((data) {
-      var updated = data;
-      if (updated != null && updated.length > 0) {
-        print(updated);
-      }
-    });
-  }
-
-  List<Color> selectedDiceList;
-  List<Widget> diceMovesList;
-
-  generateSelectedDiceList(GameState gameState) {
-    selectedDiceList = List.generate(gameState.movesList.length, (i) {
-      if (i == gameState.selectedDiceIndex) return white;
-      return trans;
-    });
-  }
-
-  generateDiceMovesList(GameState gameState, Color c) {
-    if (gameState.selectedDiceIndex > gameState.movesList.length - 1) {
-      gameState.selectedDiceIndex = 0;
-    }
-    diceMovesList = List.generate(
-      gameState.movesList.length,
-      (index) => Stack(
+  @override
+  Widget build(BuildContext context) {
+    GameState gameState = Provider.of<GameState>(context);
+    double width = MediaQuery.of(context).size.width;
+    double height =
+        MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top;
+    return SafeArea(
+      child: Stack(
         alignment: Alignment.center,
+        fit: StackFit.expand,
         children: <Widget>[
-          Icon(
-            Icons.crop_free,
-            color: selectedDiceList[index],
-            size: kSmallDiceSize + kSelectedIconSize,
-          ),
-          GestureDetector(
-            onTap: () {
-              setState(() {
-                gameState.selectedDiceIndex = index;
-              });
-            },
-            child: Dice(
-              size: kSmallDiceSize,
-              c: c,
-              num: gameState.movesList[index],
-            ),
+          GamePage(),
+          Row(
+            children: <Widget>[
+              CustomContainer(
+                c: PlayerPiece.getColor(PieceType.Blue),
+                selected: true,
+              ),
+              CustomContainer(
+                c: PlayerPiece.getColor(PieceType.Red),
+                selected: false,
+              ),
+              CustomContainer(
+                c: PlayerPiece.getColor(PieceType.Green),
+                selected: false,
+              ),
+              CustomContainer(
+                c: PlayerPiece.getColor(PieceType.Yellow),
+                selected: false,
+              ),
+            ],
           ),
         ],
       ),
     );
   }
+}
 
-  getRowChild(GameState gameState, PieceType pt) {
-    List<PlayerPiece> completedPiecesList = gameState
-        .getPlayerPieceList(pt)
-        .where((element) => element.isRunComplete()).toList();
+class CustomContainer extends StatefulWidget {
+  final Color c;
+  final Widget child;
+  bool selected;
 
-    int rowLength = completedPiecesList.length;
-    var topPiece = Container();
-    if (completedPiecesList.length == 4) {
-      --rowLength;
-      topPiece = Container(child: completedPiecesList.last.container);
-    }
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: <Widget>[
-        topPiece,
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: List.generate(
-            rowLength,
-            (index) => Container(child: completedPiecesList[index].container),
-          ),
-        ),
-      ],
-    );
-  }
+  CustomContainer({this.c = white, this.child, @required this.selected});
 
   @override
+  _CustomContainerState createState() => _CustomContainerState();
+}
+
+class _CustomContainerState extends State<CustomContainer> {
+  @override
   Widget build(BuildContext context) {
-    var gameState = Provider.of<GameState>(context);
-    Color stateColor = PlayerPiece.getColor(gameState.getTurn());
-    generateSelectedDiceList(gameState);
-    generateDiceMovesList(gameState, stateColor);
-    return SafeArea(
-      child: Container(
-        color: grey,
-        child: ConstrainedBox(
-          constraints: BoxConstraints(
-            maxHeight: (kBoxWidth) * 15,
-            maxWidth: (kBoxWidth) * 15,
-          ),
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: MediaQuery.of(context).size.height * 0.04),
-              SizedBox(
-                height: (kBoxWidth + kBoxBorderWidth * 2) * 15,
-                width: (kBoxWidth + kBoxBorderWidth * 2) * 15,
-                child: Stack(
-                  alignment: Alignment.center,
-                  children: <Widget>[
-                    Positioned(
-                      top: 0,
-                      left: 0,
-                      child: PieceHome(PieceType.Blue),
-                    ),
-                    Positioned(
-                      top: 0,
-                      child: RotatedBox(
-                        quarterTurns: 2,
-                        child: ColumnArea(piece: PieceType.Red),
-                      ),
-                    ),
-                    Positioned(
-                      top: 0,
-                      right: 0,
-                      child: PieceHome(PieceType.Red),
-                    ),
-                    Positioned(
-                      left: 0,
-                      child: RotatedBox(
-                        quarterTurns: 1,
-                        child: ColumnArea(piece: PieceType.Blue),
-                      ),
-                    ),
-                    SizedBox(
-                      height: middleAreaSize,
-                      width: middleAreaSize,
-                      child: CustomPaint(
-                        size: Size(middleAreaSize, middleAreaSize),
-                        painter: TrianglePainter(
-                            c: PlayerPiece.getColor(PieceType.Green)),
-                        child: getRowChild(gameState, PieceType.Green),
-                      ),
-                    ),
-                    RotatedBox(
-                      quarterTurns: 1,
-                      child: SizedBox(
-                        height: middleAreaSize,
-                        width: middleAreaSize,
-                        child: CustomPaint(
-                          size: Size(middleAreaSize, middleAreaSize),
-                          painter: TrianglePainter(
-                              c: PlayerPiece.getColor(PieceType.Blue)),
-                          child: getRowChild(gameState, PieceType.Blue),
-                        ),
-                      ),
-                    ),
-                    RotatedBox(
-                      quarterTurns: 2,
-                      child: SizedBox(
-                        height: middleAreaSize,
-                        width: middleAreaSize,
-                        child: CustomPaint(
-                          size: Size(middleAreaSize, middleAreaSize),
-                          painter: TrianglePainter(
-                              c: PlayerPiece.getColor(PieceType.Red)),
-                          child: getRowChild(gameState, PieceType.Red),
-                        ),
-                      ),
-                    ),
-                    RotatedBox(
-                      quarterTurns: 3,
-                      child: SizedBox(
-                        height: middleAreaSize,
-                        width: middleAreaSize,
-                        child: CustomPaint(
-                          size: Size(middleAreaSize, middleAreaSize),
-                          painter: TrianglePainter(
-                              c: PlayerPiece.getColor(PieceType.Yellow)),
-                          child: getRowChild(gameState, PieceType.Yellow),
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      right: 0,
-                      child: RotatedBox(
-                        quarterTurns: -1,
-                        child: ColumnArea(piece: PieceType.Yellow),
-                      ),
-                    ),
-                    Positioned(
-                      bottom: 0,
-                      left: 0,
-                      child: PieceHome(PieceType.Green),
-                    ),
-                    Positioned(
-                      bottom: 0,
-                      child: RotatedBox(
-                        quarterTurns: 0,
-                        child: ColumnArea(piece: PieceType.Green),
-                      ),
-                    ),
-                    Positioned(
-                      bottom: 0,
-                      right: 0,
-                      child: PieceHome(PieceType.Yellow),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 10),
-              SizedBox(
-                height: kSmallDiceSize + kSelectedIconSize,
-                width: MediaQuery.of(context).size.width,
-                child: ListView(
-                  physics: BouncingScrollPhysics(),
-                  scrollDirection: Axis.horizontal,
-                  children: diceMovesList,
-                ),
-              ),
-              SizedBox(height: 10),
-              GestureDetector(
-                onTap: gameState.diceTap,
-                child: Dice(
-                    size: 100, c: stateColor, num: gameState.lastMoveOnDice),
-              ),
-//              GestureDetector(
-//                onTap: Fire.instance.run,
-//                child: Container(
-//                  color: blue,
-//                  height: 200,
-//                  width: 200,
-//                  child: StreamBuilder(
-//                    stream: Fire.instance.gameStream('1589373131638'),
-//                    builder: (BuildContext context,
-//                        AsyncSnapshot<DocumentSnapshot> snapshot) {
-//                      if (snapshot.hasData) {
-//                        var x = snapshot.data.data['moves_list'];
-//                        return ListView(
-//                          children: [Text(x)],
-//                        );
-//                      }
-//                      return Text('nothing to show');
-//                    },
-//                  ),
-//                ),
-//              ),
-            ],
-          ),
-        ),
-      ),
+    double width = MediaQuery.of(context).size.width;
+    double height =
+        MediaQuery.of(context).size.height - MediaQuery.of(context).padding.top;
+
+    if (widget.selected) {
+      width = width * 0.85;
+    } else {
+      width = width * 0.05;
+    }
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 500),
+      width: width,
+      height: height,
+      color: widget.c,
+      child: widget.child,
     );
   }
 }
